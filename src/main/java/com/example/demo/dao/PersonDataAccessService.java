e1a95241be63e0843adb79c392979e3cb8d83a0f
sudobhat <sudarshan.bhat@siemens.com>
2022-07-19 15:06:49 +0200

    Add postgresql

--- src/main/java/com/example/demo/dao/PersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/PersonDataAccessService.java
@@ -4,6 +4,10 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import javax.swing.tree.RowMapper;
+
+import org.flywaydb.core.internal.jdbc.JdbcTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Person;
@@ -11,15 +15,29 @@ import com.example.demo.model.Person;
 @Repository("postgres")
 public class PersonDataAccessService implements PersonDao {
 
+    private final JdbcTemplate jdbcTemplate;
+    
+    @Autowired
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertPerson(UUID id, Person person) {
         // TODO Auto-generated method stub
         return 0;
     }
 
+
+
     @Override
     public List<Person> selectAllPeople() {
-        return List.of(new Person(UUID.randomUUID(), "FROM POSTGRES DB"));
+        final String sql = "SELECT id, name FROM person";
+        return jdbcTemplate.query(sql, (resultSet, i) -> {
+        UUID id = UUID. fromString (resultSet.getString ("id"));
+        String name = resultSet.getString("name");
+        return new Person(id, name);
+    });
     }
 
     @Override
